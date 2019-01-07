8f2821bfc10b5e96fd5c21d465260400a465d221
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-07 22:03:49 +0500

    Создан отдельный объект `Block` для виджетов

--- example1.py
+++ example1.py
@@ -1,27 +1,31 @@
 from tkinter import *
 
+class Block:
+    def __init__(self, master):
+        """
+        Инициализация виджетов в главном окне
+        :param master: главное окно, в котором будут располагаться виджеты
+        """
+        self.e = Entry(master, width=20)
+        self.b = Button(master, text="Преобразовать")
+        self.l = Label(master, bg="Black", fg="white", width=20)
+        self.b.bind('<Button>', self.strToSortlist)
+        self.e.pack()
+        self.b.pack()
+        self.l.pack()
+    def strToSortlist(self, event):
+        """
+        Сортирует слова в поля ввода и выводит их в нижнее окно
+        """
+        s = self.e.get()
+        s = s.split()
+        s.sort()
+        self.l["text"] = ' '.join(s)
+
 # Создать главное окно.
 root = Tk()
 
-# Создать виджеты и выполнить конфигурацию их свойств
-e = Entry(root, width=20)
-b = Button(root, text="Преобразовать")
-l = Label(root, bg="Black", fg="white", width=20)
-
-# Определить обработчики событий
-def strToSortlist(event):
-    s = e.get()
-    s = s.split()
-    s.sort()
-    l["text"] = ' '.join(s)
-
-# Определить события
-b.bind('<Button>', strToSortlist)
-
-# Расположить виджеты в главном окне
-e.pack()
-b.pack()
-l.pack()
+block = Block(root)
 
 # Запустить цикл обработки событий
 root.mainloop()
