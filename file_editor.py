6b840e00bdb746f89414118951357617fffb5353
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-10 21:55:55 +0500

    Написан скелет программы

---
+++ file_editor.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+
+def main():
+    root = Tk()
+    init_widgets()
+    root.mainloop()
+
+
+def init_widgets():
+    init_entry()
+    init_buttons()
+    init_text()
+
+
+def init_entry():
+    pass
+
+
+def init_buttons():
+    pass
+
+
+def init_text():
+    pass
+
+
+def init_scrollbar():
+    pass
+
+
+main()
