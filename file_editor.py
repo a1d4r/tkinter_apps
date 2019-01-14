52a23c327ab9798a7f46b2bae782f0775732ac27
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-14 22:38:49 +0500

    Добавлен минимальный интерфейс

--- file_editor.py
+++ file_editor.py
@@ -13,25 +13,38 @@ class MainWindow:
         self.init_text_section(master)
 
     def init_menu(self, master):
-        self.menu = Frame()
+        self.menu = Frame(master)
+        self.menu.pack(side=TOP)
         self.init_entry()
         self.init_buttons()
 
     def init_text_section(self, master):
-        self.text_section = Frame()
+        self.middle_frame = Frame(master)
+        self.bottom_frame = Frame(master)
+        self.middle_frame.pack(side=TOP)
+        self.bottom_frame.pack(side=TOP)
         self.init_text()
         self.init_scrollbars()
 
     def init_entry(self):
-        pass
+        self.entry = Entry(self.menu)
+        self.entry.pack(side=LEFT)
 
     def init_buttons(self):
-        pass
+        self.open_button = Button(self.menu, text="Открыть")
+        self.save_button = Button(self.menu, text="Сохранить")
+        self.open_button.pack(side=RIGHT)
+        self.save_button.pack(side=RIGHT)
 
     def init_text(self):
-        pass
+        self.text = Text(self.middle_frame)
+        self.text.pack(side=LEFT)
 
     def init_scrollbars(self):
-        pass
+        self.vertical_scrollbar = Scrollbar(self.middle_frame, orient=VERTICAL)
+        self.vertical_scrollbar.pack(side=RIGHT)
+        self.horizonal_scrollbar = Scrollbar(self.bottom_frame, orient=HORIZONTAL)
+        self.horizonal_scrollbar.pack()
+
 
 main()
