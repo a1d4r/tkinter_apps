62b4155caa49d26f4ecc268f14a1aa17c6f82110
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-15 23:55:57 +0500

    Добавлена функциональность скроллбарам

--- file_editor.py
+++ file_editor.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+
 
 def main():
     root = Tk()
@@ -17,32 +17,47 @@ class MainWindow:
         self.menu.pack(side=TOP, ipady=4)
         self.init_entry()
         self.init_buttons()
+        self.configure_buttons()
 
     def init_text_section(self, master):
         self.text_section = Frame(master)
         self.text_section.pack(side=BOTTOM)
         self.init_scrollbars()
         self.init_text()
+        self.configure_text()
+        self.configure_scrollbars()
 
     def init_entry(self):
         self.entry = Entry(self.menu, width=30)
         self.entry.pack(side=LEFT, padx=6)
 
     def init_buttons(self):
-        self.open_button = Button(self.menu, text="Открыть", command=self.open_file, width=8)
+        self.open_button = Button(self.menu, text="Открыть", width=8)
         self.save_button = Button(self.menu, text="Сохранить", command=self.save_file, width=8)
         self.save_button.pack(side=LEFT, padx=4)
         self.open_button.pack(side=LEFT, padx=4)
 
+    def configure_buttons(self):
+        self.open_button.config(command=self.open_file)
+        self.save_button.config(command=self.save_file)
+
     def init_text(self):
         self.text = Text(self.text_section, height=20, width=40)
         self.text.pack(side=TOP, fill=BOTH, expand=TRUE)
+
+    def configure_text(self):
+        self.text.config(xscrollcommand=self.horizonal_scrollbar.set)
+        self.text.config(yscrollcommand=self.vertical_scrollbar.set)
 
     def init_scrollbars(self):
         self.horizonal_scrollbar = Scrollbar(self.text_section, orient=HORIZONTAL)
         self.horizonal_scrollbar.pack(side=BOTTOM, fill=X, expand=FALSE)
         self.vertical_scrollbar = Scrollbar(self.text_section, orient=VERTICAL)
         self.vertical_scrollbar.pack(side=RIGHT, fill=Y, expand=FALSE)
+
+    def configure_scrollbars(self):
+        self.horizonal_scrollbar.config(command=self.text.xview)
+        self.vertical_scrollbar.config(command=self.text.yview)
 
     def open_file(self):
         self.text.delete("1.0", END)
