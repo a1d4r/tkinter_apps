b311fcb33e36df04e3d99d2962a2b8b5e585fce7
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-17 10:51:42 +0300

    Доработаны радиокнопки

--- radiobuttons.py
+++ radiobuttons.py
@@ -8,16 +8,28 @@ def main():
 
 
 class MainWindow:
+    button_values = ['Вася', 'Петя', 'Витя']
+    label_values = ['+7 900-555-33-44', '+7 960-444-22-11', '+7 940-777-55-88']
+
     def __init__(self, master):
         self.buttons = Frame(master)
         self.label = Label(master)
         self.buttons.pack(side=LEFT)
         self.label.pack(side=LEFT)
-        self.rbutton1 = Radiobutton(self.buttons)
-        self.rbutton2 = Radiobutton(self.buttons)
-        self.rbutton3 = Radiobutton(self.buttons)
+        self.init_buttons()
+
+    def init_buttons(self):
+        self.value = IntVar()
+        self.value.set(0)
+        self.rbutton1 = Radiobutton(self.buttons, variable=self.value, value=0, text=self.button_values[0])
+        self.rbutton2 = Radiobutton(self.buttons, variable=self.value, value=1, text=self.button_values[1])
+        self.rbutton3 = Radiobutton(self.buttons, variable=self.value, value=2, text=self.button_values[2])
         self.rbutton1.pack(side=TOP)
         self.rbutton2.pack(side=TOP)
         self.rbutton3.pack(side=TOP)
+
+    def update_label(self):
+        self.label["text"] = self.label_values[self.value]
+
 
 main()
