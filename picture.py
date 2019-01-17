ac6993d11c28cf247340d8a0e897da543a7d2f79
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-17 18:14:42 +0300

    Обновлен скелет программы

--- picture.py
+++ picture.py
@@ -4,12 +4,16 @@ from tkinter import *
 def main():
     root = Tk()
     p = Picture(root)
+    p.pack()
+    p.draw_picture()
     root.mainloop()
 
 
 class Picture(Canvas):
     def draw_picture(self):
-        pass
+        self.draw_sun()
+        self.draw_house()
+        self.draw_grass()
 
     def draw_sun(self):
         pass
