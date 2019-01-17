b19394fe080d17ce4444c4fd9fe675c5fb960a36
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-17 17:28:38 +0300

    Кнопка теперь меняет размеры текстового окна, текстовое окно теперь меняет цвет в зависимости от фокуса

--- text_window.py
+++ text_window.py
@@ -14,7 +14,7 @@ class MainWindow:
 
     def init_menu(self, master):
         self.menu = Frame(master)
-        self.button = Button(self.menu, text="Изменить")
+        self.button = Button(self.menu, text="Изменить", command=lambda: self.resize(None))
         self.entry_height = Entry(self.menu, width=3)
         self.entry_width = Entry(self.menu, width=3)
         self.menu.pack(side=TOP)
@@ -22,9 +22,27 @@ class MainWindow:
         self.entry_height.pack(side=TOP)
         self.entry_width.pack(side=BOTTOM)
 
+        self.button.bind("<Return>", self.resize)
+        self.entry_height.bind("<Return>", self.resize)
+        self.entry_width.bind("<Return>", self.resize)
+
+    def resize(self, event):
+        new_height = int(self.entry_height.get())
+        new_width = int(self.entry_width.get())
+        self.text.configure(height=new_height, width=new_width)
+
     def init_text(self, master):
         self.text = Text(master, height=10, width=20)
         self.text.pack(side=TOP)
+
+        self.text.bind('<FocusIn>', self.paint_in_white)
+        self.text.bind('<FocusOut>', self.paint_in_gray)
+
+    def paint_in_gray(self, event):
+        self.text["bg"] = "lightgray"
+
+    def paint_in_white(self, event):
+        self.text["bg"] = "white"
 
 
 
