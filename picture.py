8d578b5bd55b10c680d29cadbf6e716d84933869
a1d4r <40607719+a1d4r@users.noreply.github.com>
2019-01-17 18:13:34 +0300

    Написан скелет программы

---
+++ picture.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+def main():
+    root = Tk()
+    p = Picture(root)
+    root.mainloop()
+
+
+class Picture(Canvas):
+    def draw_picture(self):
+        pass
+
+    def draw_sun(self):
+        pass
+
+    def draw_house(self):
+        pass
+
+    def draw_grass(self):
+        pass
+
+
+main()
